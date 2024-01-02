021658aae328747736427146ed0908204a190127
Marek Kacprzak <kacprzakmarek92@gmail.com>
2024-01-02 20:02:26 +0100

    Add instance folder creation in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,11 @@ import os
 def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    
