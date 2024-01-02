d436149daea1b3f77971d0baff5297fee528cad2
Marek Kacprzak <kacprzakmarek92@gmail.com>
2024-01-02 23:50:39 +0100

    Added the creation of the unique conversation_id for the client

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+import secrets
 
 
 main_bp = Blueprint("main_bp", __name__)
@@ -6,4 +7,10 @@ main_bp = Blueprint("main_bp", __name__)
 
 @main_bp.route("/")
 def index():
-    return "Hello, world!"
+    return "Hello! Welcome to the most secure comunication app in the world!"
+
+
+@main_bp.route("/connect-to-server", methods=["POST"])
+def connect_to_server():
+    # Create a unique conversation_id for the client
+    conversation_id = secrets.token_hex(32)
