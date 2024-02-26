3db2128693711be8d6d7217a8d39c1e354a9d8d6
NaviteLogger <kacprzakmarek92@gmail.com>
2024-02-26 17:37:10 +0100

    Added client-side folder for future development

--- app/communicator/routes.py
+++ app/communicator/routes.py
@@ -1,2 +1,9 @@
 from flask import Blueprint, request, jsonify
 
+communicator_bp = Blueprint("communicator", __name__)
+
+
+@communicator_bp.route("/communicator", methods=["POST"])
+def communicator():
+    data = request.get_json()
+    return jsonify(data)
